df627d8ff504d1946cf32132c6bf26a746b156cf
a.moinur <a.moinur@a1qa.com>
2024-06-03 14:34:13 +0600

    Improved codes for API commit

--- framework/tests/conftest.py
+++ framework/tests/conftest.py
@@ -1,14 +1,7 @@
 import pytest
-from browser.py_quality_services import PyQualityServices
-from framework.utils.browser_factory import BrowserFactory
 from framework.utils.spotify_search import SpotifyApiSearch
 
 
 @pytest.fixture(scope="session")
 def spotify_api_client():
     return SpotifyApiSearch()
-
-
-def pytest_sessionstart(session):
-    PyQualityServices.browser_factory = BrowserFactory()
-    PyQualityServices.get_browser()
